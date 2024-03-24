fec4d07c332f8e722a090d66e7b941c683f1721d
Stephen Aranda <aranda.stephen88@gmail.com>
2024-03-23 19:51:51 -0500

    added p elements for the home page paragraph

--- bitcoin-transaction-system/src/Components/Home.js
+++ bitcoin-transaction-system/src/Components/Home.js
@@ -9,6 +9,12 @@ const Home = () => {
                 <main className="main-doc">
                     <h1>Welcome to Bitcoin Transaction System</h1>
 
+                    <p>Here at Bitcoin Transaction System, we provide an online platform to buy and sell bitcoin.</p>
+                    <p>Whether you're a sophisticated and savvy investor looking to directly purchase and sell</p>
+                    <p>or you're a trader that has to take care of clients bitcoin investment needs,</p>
+                    <p>this application will provide an easy-to-use user interface and other</p>
+                    <p>utilities to make investment choices in bitcoin easy and seamless.</p>
+
                 </main>
 
 
