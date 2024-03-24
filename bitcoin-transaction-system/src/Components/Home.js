b6458858ab2d3515c7ceb668a234dde0287c1c96
Stephen Aranda <aranda.stephen88@gmail.com>
2024-03-23 23:04:22 -0500

    tested image placement

--- bitcoin-transaction-system/src/Components/Home.js
+++ bitcoin-transaction-system/src/Components/Home.js
@@ -5,17 +5,22 @@ const Home = () => {
     return (
         <div>
             <div className="home-container">
-                <div className="app-img"></div>
-                <main className="main-doc">
-                    <h1>Welcome to Bitcoin Transaction System</h1>
 
-                    <p>Here at Bitcoin Transaction System, we provide an online platform to buy and sell bitcoin.</p>
-                    <p>Whether you're a sophisticated and savvy investor looking to directly purchase and sell</p>
-                    <p>or you're a trader that has to take care of clients bitcoin investment needs,</p>
-                    <p>this application will provide an easy-to-use user interface and other</p>
-                    <p>utilities to make investment choices in bitcoin easy and seamless.</p>
+
+                <main className="main-doc">
+                    <div className="main-text">
+                        <h1>Welcome to Bitcoin Transaction System</h1>
+
+                        <p>Here at Bitcoin Transaction System, we provide an online platform to buy and sell bitcoin.</p>
+                        <p>Whether you're a sophisticated and savvy investor looking to directly purchase and sell</p>
+                        <p>or you're a trader that has to take care of clients bitcoin investment needs,</p>
+                        <p>this application will provide an easy-to-use user interface and other</p>
+                        <p>utilities to make investment choices in bitcoin easy and seamless.</p>
+                    </div>
                 </main>
-                <div className="trading-img"></div>
+
+
+
 
 
             </div>
