0e2e5cc70acc3b7144d8d5b93333b4178448c4e7
Stephen Aranda <aranda.stephen88@gmail.com>
2024-03-23 23:04:05 -0500

    Added img and card text

--- bitcoin-transaction-system/src/App.js
+++ bitcoin-transaction-system/src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "../src/Styles/App.css";
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom'
 import Home from '../src/Components/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Switch>
-          <Route exact path="/" component={Home}></Route>
-        </Switch>
+        <Routes>
+          <Route exact path="/" Component={Home}></Route>
+        </Routes>
 
       </header>
     </div>
