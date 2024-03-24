8f276dc48590e7eb3df834b3bffe4683eb28491b
Stephen Aranda <aranda.stephen88@gmail.com>
2024-03-23 19:37:19 -0500

    Added div to contain elements of Home Page

--- bitcoin-transaction-system/src/Components/Home.js
+++ bitcoin-transaction-system/src/Components/Home.js
@@ -2,5 +2,12 @@ import React from "react";
 import "../Styles/Home.css";
 
 const Home = () => {
+    return (
+        <div>
+            <div className="home-container">
+
+            </div>
+        </div>
+    )
 
 }
