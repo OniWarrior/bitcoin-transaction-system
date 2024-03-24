6e46f61aff6eb476c49f7e287edef0718d74880d
Stephen Aranda <aranda.stephen88@gmail.com>
2024-03-23 20:20:28 -0500

    Added Route for home page

--- bitcoin-transaction-system/src/App.js
+++ bitcoin-transaction-system/src/App.js
@@ -1,9 +1,15 @@
-
+import React from "react";
+import "../src/Styles/App.css";
+import { Switch, Route } from "react-router-dom";
+import Home from '../src/Components/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+        </Switch>
 
       </header>
     </div>
