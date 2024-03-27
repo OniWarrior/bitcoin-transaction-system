df7c82f1870b1bc46523d819870364e15a9f4098
Stephen Aranda <aranda.stephen88@gmail.com>
2024-03-26 21:40:10 -0500

    created useValidation hook

--- bitcoin-transaction-system/src/FormSchemas/useValidation.js
+++ bitcoin-transaction-system/src/FormSchemas/useValidation.js
@@ -2,7 +2,17 @@ import { useState } from 'react'
 import * as yup from 'yup'
 
 
-const initialValue = {
+const initialValues = {
     email: '',
     password: ''
+}
+
+const initialErrors = {
+    email: '',
+    password: ''
+}
+
+export const useValidation = (schema) => {
+    const [data, setData] = useState(initialValues)
+
 }
