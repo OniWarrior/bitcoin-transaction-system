fd2904fec3dce40f17abada3ae5ec2b9f759442d
Stephen Aranda <aranda.stephen88@gmail.com>
2024-03-31 21:28:03 -0500

    create state variables

--- bitcoin-transaction-system/src/Components/Login.js
+++ bitcoin-transaction-system/src/Components/Login.js
@@ -2,12 +2,28 @@ import React from 'react'
 import '../Styles/Login.css'
 import { Link } from 'react-router-dom'
 import UnsignedNavigation from './UnsignedNavigation'
+import LoginFormSchema from '../FormSchemas/LoginFormSchema'
+import { useValidation } from '../Hooks/useValidation.js'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { connect } from 'react-redux'
+
 
 
 
 
 
 const Login = (props) => {
+    const navigate = useNavigate()
+    const [login, errors, setLogin] = useValidation(LoginFormSchema)
+    const initialDisabled = true
+    const [disabled, setDisabled] = useState(initialDisabled)
+
+    const change = (event) => {
+        setLogin(event, LoginFormSchema)
+    }
+
+
 
 
 
