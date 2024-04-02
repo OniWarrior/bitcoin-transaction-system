839e3aa8feedafed51683a62be9c1b05f695c6bb
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-02 14:43:19 -0500

    completed login reducer function

--- bitcoin-transaction-system/src/State/Reducers/LoginReducer.js
+++ bitcoin-transaction-system/src/State/Reducers/LoginReducer.js
@@ -10,3 +10,17 @@ const initialState = {
     error: ''
 }
 
+const loginReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case LOGIN_START:
+            return { ...state, loading: true }
+        case LOGIN_SUCCESS:
+            return { ...state, login: action.payload, loading: false }
+        case LOGIN_FAILURE:
+            return { ...state, error: action.payload, loading: false }
+        default:
+            return state;
+    }
+}
+
+export default loginReducer
