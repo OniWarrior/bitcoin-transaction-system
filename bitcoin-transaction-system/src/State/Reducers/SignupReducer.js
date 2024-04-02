7a7c0aca849611042ff5e258577926654fd9b356
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-02 14:49:19 -0500

    completed signup reducer function

--- bitcoin-transaction-system/src/State/Reducers/SignupReducer.js
+++ bitcoin-transaction-system/src/State/Reducers/SignupReducer.js
@@ -11,3 +11,17 @@ const initialState = {
     error: ''
 }
 
+const signupReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SIGNUP_START:
+            return { ...state, loading: true }
+        case SIGNUP_SUCCESS:
+            return { ...state, signup: action.payload, loading: false }
+        case SIGNUP_FAILURE:
+            return { ...state, error: action.payload, loading: false }
+        default:
+            return;
+    }
+}
+
+export default signupReducer
