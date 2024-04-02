35b7240cf07b14980b5d77e3b8820adee1f70e3b
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-02 12:34:09 -0500

    coded successful response

--- bitcoin-transaction-system/src/State/Actions/LoginActions.js
+++ bitcoin-transaction-system/src/State/Actions/LoginActions.js
@@ -4,3 +4,13 @@ export const LOGIN_START = 'LOGIN_START'
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
 export const LOGIN_FAILURE = 'LOGIN_FAILURE'
 
+export const postLogin = (login, navigate) => (dispatch) => {
+
+    dispatch({ type: LOGIN_START })
+    axios.post('TODO HAVE TO SET UP HEROKU', login)
+        .then(response => {
+            dispatch({ type: LOGIN_SUCCESS, payload: response.data })
+            localStorage.setItem('token', response.data.token)
+            navigate('/Dashboard')
+        })
+}
