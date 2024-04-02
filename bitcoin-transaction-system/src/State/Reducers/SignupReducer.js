0e502f2dd90a6f3b092dd1f3106e0e63960b7355
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-02 14:45:58 -0500

    created initial state object

---
+++ bitcoin-transaction-system/src/State/Reducers/SignupReducer.js
@@ -0,0 +1,13 @@
+import {
+    SIGNUP_START,
+    SIGNUP_SUCCESS,
+    SIGNUP_FAILURE
+} from '../Actions/SignupActions.js'
+
+
+const initialState = {
+    signup: {},
+    loading: false,
+    error: ''
+}
+
