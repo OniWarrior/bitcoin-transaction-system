39bc32e596766779f50ea22c2fe90a9258f033c3
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-19 15:24:19 -0500

    finished placing base url and headers

--- bitcoin-transaction-system/src/Components/Utils/AxiosWithAus.js
+++ bitcoin-transaction-system/src/Components/Utils/AxiosWithAus.js
@@ -1,6 +1,12 @@
 import axios from 'axios'
 
 const axiosWithAus = () => {
+    return axios.create({
+        baseURL: 'https://bitcoin-transaction-system-be-72349974fde7.herokuapp.com/',
+        headers: {
+            authorization: localStorage.getItem('token')
+        }
+    })
 
 }
 
