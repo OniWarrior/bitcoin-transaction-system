9352aa3753c58c5637fa238e834cc2bbdb94b3b9
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-19 15:28:04 -0500

    created blank func for protected route

---
+++ bitcoin-transaction-system/src/Components/Utils/ProtectedRoute.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Route, Redirect } from 'react-router'
+
+const ProtectedRoute = ({ Component, ...rest }) => {
+
+}
+
+export default ProtectedRoute
