76baf0e5fafd4e6cadc728560c04fcd71ef36a08
Williamnetgear <williamnetgear@yahoo.com>
2024-04-19 21:36:55 -0500

    created state cars for signup and errors

--- bitcoin-transaction-system/src/Components/Signup.js
+++ bitcoin-transaction-system/src/Components/Signup.js
@@ -5,6 +5,7 @@ import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
     const navigate = useNavigate();
+    const [signup, error, setSignup] = useValidation(SignupFormSchema);
 }
 
 export default Signup;
