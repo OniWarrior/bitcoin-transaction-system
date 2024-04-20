cb9540f89c27b88f5e599cf99b0f8b8317ed7f62
Williamnetgear <williamnetgear@yahoo.com>
2024-04-20 13:33:16 -0500

    created onFormSubmit to register accounts

--- bitcoin-transaction-system/src/Components/Signup.js
+++ bitcoin-transaction-system/src/Components/Signup.js
@@ -22,7 +22,7 @@ const Signup = () => {
         e.preventDefault()
         if(signup.firstName.length > 0 && signup.firstName.length <= 20 &&
             signup.lastName.length > 0 && signup.lastName.length <= 20 && 
-            signup.phoneNumber.length == 10 && signup.cellNum.length == 10 && 
+            signup.phoneNumber.length >= 10 && signup.cellNum.length >= 10 && 
             signup.streetAddress.length <= 20 && signup.city.length <= 20 && 
             signup.state.length <= 15 && signup.userType && 
             signup.email.length <= 30 && signup.password.length >= 5)
@@ -36,6 +36,13 @@ const Signup = () => {
                 disabled: disabled
             }))
         }
+    }
+
+    // send the values of signup and navigate to api call to
+    // post signup to back end
+    // In other words, register new account
+    const onFormSubmit = () => {
+        props.register(signup, navigate)
     }
 }
 
