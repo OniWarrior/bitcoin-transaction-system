602149ea893197d61bfafd3a0754ba342702e2f0
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-20 20:53:38 -0500

    completed reducer

--- bitcoin-transaction-system/src/State/Reducers/ManagerReducer.js
+++ bitcoin-transaction-system/src/State/Reducers/ManagerReducer.js
@@ -10,3 +10,21 @@ const initialState = {
     error: ''
 
 }
+
+
+const managerReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "MANAGER_START":
+            return { ...state, loading: true }
+        case 'MANAGER_SUCCESS':
+            return { ...state, totalTransactions: action.payload, loading: false }
+        case 'MANAGER_FAILURE':
+            return { ...state, error: action.payload, loading: false }
+        default:
+            return state
+
+    }
+
+}
+
+export default managerReducer
