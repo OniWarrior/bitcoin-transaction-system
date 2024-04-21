ca14c4828895eed9a1c33ea14034e63338c6bdff
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-20 21:37:54 -0500

    mapped dispatch and state to props

--- bitcoin-transaction-system/src/Components/Manager/ManagerRetrieveTotDailyTransacs.js
+++ bitcoin-transaction-system/src/Components/Manager/ManagerRetrieveTotDailyTransacs.js
@@ -1,7 +1,26 @@
+import React from "react"
+import { getTotalDailyTransactions } from "../../State/Actions/ManagerActions"
+import SignedInNavigation from "../SignedInNavigation"
+import { connect } from "react-redux"
+import { useNavigate } from "react-router"
 
 
-const RetrieveTotDailyTransacs = () => {
+const RetrieveTotDailyTransacs = (props) => {
 
 }
 
-export default RetrieveTotDailyTransacs
+
+
+const mapStateToProps = (state) => {
+    return {
+        totalTransactions: state.managerReducer.totalTransactions,
+        loading: state.managerReducer.loading,
+        error: state.managerReducer.error
+    }
+}
+
+const mapDispatchToProps = { getTotalDailyTransactions }
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(RetrieveTotDailyTransacs)
