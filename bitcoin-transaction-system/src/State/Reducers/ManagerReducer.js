3e7b4fd977edbb288b2548ad6b05dff577d4de17
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-20 20:48:10 -0500

    created initial state

---
+++ bitcoin-transaction-system/src/State/Reducers/ManagerReducer.js
@@ -0,0 +1,12 @@
+import {
+    MANAGER_START,
+    MANAGER_SUCCESS,
+    MANAGER_FAILURE
+} from '../Actions/ManagerActions'
+
+const initialState = {
+    totalTransactions: '',
+    loading: false,
+    error: ''
+
+}
