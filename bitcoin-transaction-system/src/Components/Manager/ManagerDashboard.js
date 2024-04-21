c9511cdecfebfa5f5bde36c1789c80b61d1035f2
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-20 21:07:20 -0500

    created style file and created handler for to daily

--- bitcoin-transaction-system/src/Components/Manager/ManagerDashboard.js
+++ bitcoin-transaction-system/src/Components/Manager/ManagerDashboard.js
@@ -1,7 +1,19 @@
-
+import React from "react"
+import '../../Styles/Manager.css'
+import { useNavigate } from "react-router"
+import SignedInNavigation from '../SignedInNavigation'
 
 
 const ManagerDashboard = () => {
+
+    const navigate = useNavigate()
+
+    const goToDaily = (e) => {
+        e.preventDefault()
+        navigate('/ManagerDashboard/total-daily-transactions')
+    }
+
+
 
 }
 
