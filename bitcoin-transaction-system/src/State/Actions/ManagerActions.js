808f8dab2a57f4f8fd489dc651284386a6a2590e
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-20 20:34:39 -0500

    created successful response and failure response

--- bitcoin-transaction-system/src/State/Actions/ManagerActions.js
+++ bitcoin-transaction-system/src/State/Actions/ManagerActions.js
@@ -1,6 +1,22 @@
 import axios from "axios";
 
-const MANAGER_START = 'MANAGER_START'
-const MANAGER_SUCCESS = 'MANAGER_SUCCESS'
-const MANAGER_FAILURE = 'MANAGER_FAILURE'
+export const MANAGER_START = 'MANAGER_START'
+export const MANAGER_SUCCESS = 'MANAGER_SUCCESS'
+export const MANAGER_FAILURE = 'MANAGER_FAILURE'
 
+export const getTotalDailyTransactions = (date, navigate) => (dispatch) => {
+
+    dispatch({ type: MANAGER_START })
+    axios.get('/api/user/total-daily-transactions', date)
+        .then(response => {
+            dispatch({ type: MANAGER_START, payload: response.data })
+
+
+        })
+        .catch(err => {
+            dispatch({ type: MANAGER_FAILURE, payload: err.message })
+        })
+
+
+
+}
