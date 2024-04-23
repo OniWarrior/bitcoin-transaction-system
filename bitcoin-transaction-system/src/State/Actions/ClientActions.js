a4f4a598e835cad22b9570f4c9f56173a0c89823
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-23 13:51:16 -0500

    created successful response

--- bitcoin-transaction-system/src/State/Actions/ClientActions.js
+++ bitcoin-transaction-system/src/State/Actions/ClientActions.js
@@ -4,3 +4,14 @@ export const CLIENT_START = 'CLIENT_START'
 export const CLIENT_SUCCESS = 'CLIENT_SUCCESS'
 export const CLIENT_FAILURE = 'CLIENT_FAILURE'
 
+// api call to retrieve bitcoin wallet of the client
+
+
+export const getBitcoinWallet = () => (dispatch) => {
+    dispatch({ type: CLIENT_START })
+    axiosWithAuth().get('/api/users/BitcoinWallet')
+        .then(response => {
+            dispatch({ type: CLIENT_SUCCESS, payload: response.data })
+        })
+
+}
