31380943f728a6a2a6311c417d0bcd6f0cde5862
CabanDiego <majy2098@gmail.com>
2024-04-24 12:57:01 -0500

    Completed Client Reducer function

--- bitcoin-transaction-system/src/State/Reducers/ClientReducer.js
+++ bitcoin-transaction-system/src/State/Reducers/ClientReducer.js
@@ -11,9 +11,17 @@ const initialState={
     error:''
 }
 
-const clientReducer=(state = initialState,action)=>{
-    
+const clientReducer=(state = initialState, action)=>{
+    switch(action.type){
+        case CLIENT_START:
+            return{...state, loading: true}
+        case CLIENT_SUCCESS:
+            return{...state, client: action.payload, loading: false}
+        case CLIENT_FAILURE:
+            return{...state, error: action.payload, loading: false}
+        default:
+            return state;
+    }
 }
-
 
 export default clientReducer
