8c375be74a8928e9c22575ac268e136bfd7de49e
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-25 13:44:24 -0500

    add button hover animation

--- bitcoin-transaction-system/src/Components/Signup.js
+++ bitcoin-transaction-system/src/Components/Signup.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useValidation } from '../Hooks/useValidation.js'
+import { useSignupValidation } from '../Hooks/useSignupValidation.js'
 import SignupFormSchema from '../FormSchemas/SignupFormSchema.js'
 
 
 
-const Signup = () => {
+const Signup = (props) => {
     // local variables
     const navigate = useNavigate()
-    const [signup, error, setSignup] = useValidation(SignupFormSchema)
+    const [signup, error, setSignup] = useSignupValidation(SignupFormSchema)
     const initialDisabled = true
     const [disabled, setDisabled] = useState(initialDisabled)
 
@@ -20,18 +20,17 @@ const Signup = () => {
     // handler for handling submit button
     const handleDisabled = (e) => {
         e.preventDefault()
-        if(signup.firstName.length > 0 && signup.firstName.length <= 20 &&
-            signup.lastName.length > 0 && signup.lastName.length <= 20 && 
-            signup.phoneNumber.length >= 10 && signup.cellNum.length >= 10 && 
-            signup.streetAddress.length <= 20 && signup.city.length <= 20 && 
-            signup.state.length <= 15 && signup.userType && 
-            signup.email.length <= 30 && signup.password.length >= 5)
-        {
+        if (signup.firstName.length > 0 && signup.firstName.length <= 20 &&
+            signup.lastName.length > 0 && signup.lastName.length <= 20 &&
+            signup.phoneNumber.length >= 10 && signup.cellNum.length >= 10 &&
+            signup.streetAddress.length <= 20 && signup.city.length <= 20 &&
+            signup.state.length <= 15 && signup.userType &&
+            signup.email.length <= 30 && signup.password.length >= 5) {
             setDisabled(() => ({
                 disabled: !disabled
             }))
         }
-        else{
+        else {
             setDisabled(() => ({
                 disabled: disabled
             }))
--- bitcoin-transaction-system/src/State/Actions/LoginActions.js
+++ bitcoin-transaction-system/src/State/Actions/LoginActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+import { jwtDecode } from 'jwt-decode'
 export const LOGIN_START = 'LOGIN_START'
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
 export const LOGIN_FAILURE = 'LOGIN_FAILURE'
@@ -13,7 +13,26 @@ export const postLogin = (login, navigate) => (dispatch) => {
             dispatch({ type: LOGIN_SUCCESS, payload: response.data })
             localStorage.setItem('token', response.data.token)
 
-            navigate('/ManagerDashboard')
+            // decoded token
+            const decoded = jwtDecode(response.data.token)
+
+            // decide which dashboard to redirect to based
+            // on user type
+            switch (decoded.user_type) {
+                case 'Client': navigate('/ClientDashboard')
+                    break;
+                case 'Trader': navigate('/TraderDashboard')
+                    break;
+                case 'Manager': navigate('/ManagerDashboard')
+                    break;
+                default:
+                    return;
+            }
+
+
+
+
+
         })
         .catch(err => {
 
