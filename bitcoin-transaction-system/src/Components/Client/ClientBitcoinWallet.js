6bb0b32765ff26d7a5d13844a7a1588488305aee
Williamnetgear <williamnetgear@yahoo.com>
2024-04-25 15:08:04 -0500

    initialized mapStateToProps

--- bitcoin-transaction-system/src/Components/Client/ClientBitcoinWallet.js
+++ bitcoin-transaction-system/src/Components/Client/ClientBitcoinWallet.js
@@ -4,8 +4,12 @@
 const BitcoinWallet = (props) => {
 
 }
-const mapStateToProps=(state)=>{
-
+const mapStateToProps = (state) => {
+    return {
+        client: state.clientReducer.client,
+        loading: state.clientReducer.loading,
+        error: state.clientReducer.error
+    }
 }
 
 export default BitcoinWallet;
