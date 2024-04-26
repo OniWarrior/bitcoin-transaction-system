d0a7db05201d9cf5673716b713fcdfd5d667b2d2
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-25 20:47:34 -0500

    completed validation hook

---
+++ bitcoin-transaction-system/src/Hooks/useTransferValidation.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import * as yup from 'yup'
+
+const initialValues = {
+    transfer_amount: ''
+
+}
+
+const initialErrors = {
+    transfer_amount: ''
+
+}
+
+export const useTransferValidation = (schema) => {
+    const [data, setData] = useState(initialValues)
+    const [errors, setErrors] = useState(initialErrors)
+
+    const onInputChange = (event) => {
+        const { name, value } = event.target
+        yup.reach(schema, name)
+            .validate(value)
+            .then(() => {
+                setErrors({ ...errors, [name]: '' })
+
+            })
+            .catch((err) => {
+                setErrors({ ...errors, [name]: err.errors[0] })
+            })
+        setData({
+            ...data,
+            [name]: value
+
+
+        })
+
+    }
+
+    return [data, errors, onInputChange]
+
+}
