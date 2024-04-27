d82080dc23bd03287c55b8e1b20baefac47ecd72
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-26 21:05:42 -0500

    wired cancel log to state

--- bitcoin-transaction-system/src/Components/Trader/TraderCancelLog.js
+++ bitcoin-transaction-system/src/Components/Trader/TraderCancelLog.js
@@ -1,7 +1,17 @@
+import { connect } from "react-redux"
+import SignedInNavigation from "../SignedInNavigation"
 
 
-const CancelLog = () => {
+const CancelLog = (props) => {
 
 }
 
-export default CancelLog
+const mapStateToProps = (state) => {
+    return {
+        trader: state.traderReducer.trader,
+        loading: state.traderReducer.loading,
+        error: state.traderReducer.error
+    }
+}
+
+export default connect(mapStateToProps)(CancelLog)
