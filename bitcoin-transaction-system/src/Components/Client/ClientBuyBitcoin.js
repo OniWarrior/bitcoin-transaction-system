bee1f28d2b6039b530766df4265bdb8663f3805b
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-27 15:58:45 -0500

    created handlers

--- bitcoin-transaction-system/src/Components/Client/ClientBuyBitcoin.js
+++ bitcoin-transaction-system/src/Components/Client/ClientBuyBitcoin.js
@@ -1,11 +1,41 @@
 import { connect } from "react-redux"
 import { useNavigate } from "react-router"
 import { postBuyBitcoin } from "../../State/Actions/ClientActions"
+import ClientBuyBitcoinFormSchema from '../../FormSchemas/ClientBuyBitcoinFormSchema'
+import { useBuyBitcoinValidation } from "../../Hooks/useBuyBitcoinValidation"
+import { useState } from "react"
 
 
 const BuyBitcoin = (props) => {
     const navigate = useNavigate()
-    const [order, errors, SetOrder] =
+    const [order, errors, SetOrder] = useBuyBitcoinValidation(ClientBuyBitcoinFormSchema)
+    const initialDisabled = true
+    const [disabled, setDisabled] = useState(initialDisabled)
+
+    const change = (e) => {
+        e.preventDefault()
+        SetOrder(e, ClientBuyBitcoinFormSchema)
+    }
+
+    const handleDisabled = (event) => {
+        event.preventDefault()
+        if (order.Bitcoin_balance > 0) {
+            setDisabled(() => ({
+                disabled: !disabled
+            }))
+        }
+        else {
+            setDisabled(() => ({
+                disabled: disabled
+            }))
+        }
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault()
+        props.postBuyBitcoin(order, navigate)
+
+    }
 
 }
 
