a5811021c204b6a6098a44e66026c128a675caab
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-27 15:52:34 -0500

    completed hook

---
+++ bitcoin-transaction-system/src/Hooks/useBuyBitcoinValidation.js
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import * as yup from 'yup'
+
+const initialValues = {
+    Bitcoin_balance: '',
+    comm_type: ''
+}
+
+const initialErrors = {
+    Bitcoin_balance: '',
+    comm_type: ''
+}
+
+
+export const useBuyBitcoinValidation = (schema) => {
+    const [data, setData] = useState(initialValues)
+    const [errors, setErrors] = useState(initialErrors)
+
+
+    const onInputChange = (event) => {
+        const { name, value } = event.target
+        yup.reach(schema, name)
+            .validate(value)
+            .then(() => {
+                setErrors({ ...errors, [name]: "" })
+            })
+            .catch(err => {
+                setErrors({ ...errors, [name]: err.errors[0] })
+            })
+
+        setData({
+            ...data,
+            [name]: value
+        })
+
+    };
+
+    return [data, errors, onInputChange]
+
+}
