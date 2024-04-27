afb413d73f600ac18dfca6e4f7d5404710426acd
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-26 20:02:14 -0500

    completed reducer function

--- bitcoin-transaction-system/src/State/Reducers/TraderReducer.js
+++ bitcoin-transaction-system/src/State/Reducers/TraderReducer.js
@@ -11,3 +11,16 @@ const initialState = {
 }
 
 
+export const traderReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case TRADER_START:
+            return { ...state, loading: true }
+        case TRADER_SUCCESS:
+            return { ...state, trader: action.payload, loading: false }
+        case TRADER_FAILURE:
+            return { ...state, error: action.payload, loading: false }
+        default:
+            return state;
+    }
+
+}
