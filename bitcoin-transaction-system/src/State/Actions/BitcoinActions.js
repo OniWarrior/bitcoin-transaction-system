434b93770b4dfb4e2d7d58b4ebe86c9383221ef7
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-27 15:35:28 -0500

    created api call

--- bitcoin-transaction-system/src/State/Actions/BitcoinActions.js
+++ bitcoin-transaction-system/src/State/Actions/BitcoinActions.js
@@ -6,8 +6,8 @@ export const BITCOIN_FAILURE = 'BITCOIN_FAILURE'
 
 
 
-// third party api call to fetch bitcoin and other cryptocurrency info
-export const fetchLatestCryptocurrency = (navigate) => (dispatch) => {
+// third party api call to fetch bitcoin and other cryptocurrency info for Client
+export const fetchLatestCryptocurrencyForClient = (navigate) => (dispatch) => {
     dispatch({ type: BITCOIN_START });
 
     const config = {
@@ -19,21 +19,34 @@ export const fetchLatestCryptocurrency = (navigate) => (dispatch) => {
     axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', config)
         .then(response => {
             dispatch({ type: BITCOIN_SUCCESS, payload: response.data });
-            localStorage.setItem('token', response.data.token)
 
-            // decoded token
-            const decoded = jwtDecode(response.data.token)
+            navigate('/ClientDashboard/BuyBitcoin')
 
-            // decide which dashboard to redirect to based
-            // on user type
-            switch (decoded.user_type) {
-                case 'Client': navigate('/ClientDashboard/BuyBitcoin')
-                    break;
-                case 'Trader': navigate('/TraderDashboard/TraderClientSearch/clients/:client_id/TraderBuyBitcoin')
-                    break;
-                default:
-                    return;
-            }
+
+
+        })
+        .catch(error => {
+            dispatch({ type: BITCOIN_FAILURE, payload: error.message });
+        });
+}
+
+
+
+export const fetchLatestCryptocurrencyForTrader = (navigate, clientId) => (dispatch) => {
+    dispatch({ type: BITCOIN_START });
+
+    const config = {
+        headers: {
+            'X-CMC_PRO_API_KEY': 'a6f492ca-a732-4331-976f-e9a31e270372',
+        }
+    };
+
+    axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', config)
+        .then(response => {
+            dispatch({ type: BITCOIN_SUCCESS, payload: response.data });
+            navigate(`/TraderClientSearch/clients/${clientId}/TraderBuyBitcoin`)
+
+
 
         })
         .catch(error => {
