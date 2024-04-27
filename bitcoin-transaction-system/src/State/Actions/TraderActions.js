1fc6b2d373ac7e4e73a384aadb249bef998ca8ca
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-26 20:15:16 -0500

    finished success and failure response

--- bitcoin-transaction-system/src/State/Actions/TraderActions.js
+++ bitcoin-transaction-system/src/State/Actions/TraderActions.js
@@ -11,5 +11,14 @@ export const TRADER_FAILURE = 'TRADER_FAILURE'
 // api call to retrieve all records in the cancel log table
 export const getCancelLog = (navigate) => (dispatch) => {
     dispatch({ type: TRADER_START })
+    axiosWithAuth().get('api/users/cancel-log')
+        .then(response => {
+            dispatch({ type: TRADER_SUCCESS, payload: response.data })
+            navigate('/TraderDashboard/Cancel-Log')
+
+        })
+        .catch(err => {
+            dispatch({ type: TRADER_FAILURE, payload: err.message })
+        })
 
 }
