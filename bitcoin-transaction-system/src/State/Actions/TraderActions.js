82ceaf335a5e337ffdc35655d134becbbaca5bd5
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-26 19:55:04 -0500

    created strings for dispatch to action

---
+++ bitcoin-transaction-system/src/State/Actions/TraderActions.js
@@ -0,0 +1,6 @@
+import axiosWithAuth from "../../Components/Utils/AxiosWithAuth";
+
+export const TRADER_START = 'TRADER_START'
+export const TRADER_SUCCESS = 'TRADER_SUCCESS'
+export const TRADER_FAILURE = 'TRADER_FAILURE'
+
