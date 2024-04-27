20a1fe3b192ed7ad91f9f50fba1f5746f1095648
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-26 21:34:29 -0500

    finished building cancel log list

--- bitcoin-transaction-system/src/Components/Trader/TraderCancelLog.js
+++ bitcoin-transaction-system/src/Components/Trader/TraderCancelLog.js
@@ -1,8 +1,27 @@
 import { connect } from "react-redux"
 import SignedInNavigation from "../SignedInNavigation"
-
+import CancelCard from '../Trader/CancelCard'
 
 const CancelLog = (props) => {
+
+    if (!props.trader || props.trader.length === 0) {
+        return <div>No Cancelled  Orders Or Transfers</div>
+    }
+
+    return (
+        <div className="cancelled-container">
+            <SignedInNavigation />
+            <div className="cancelled-list">
+                <h1>Cancelled Orders Or Transfers</h1>
+                {
+                    props.trader.map(cancelled => (
+                        <CancelCard key={cancelled.log_id} trader={cancelled} />
+                    ))
+                }
+            </div>
+
+        </div>
+    )
 
 }
 
