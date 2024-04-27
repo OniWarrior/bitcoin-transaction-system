e0c2ee562342fd794d479efd35bbae6be6252395
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-27 15:50:29 -0500

    finished form schema

--- bitcoin-transaction-system/src/FormSchemas/ClientBuyBitcoinFormSchema.js
+++ bitcoin-transaction-system/src/FormSchemas/ClientBuyBitcoinFormSchema.js
@@ -6,7 +6,12 @@ const ClientBuyBitcoinFormSchema = yup.object().shape({
     Bitcoin_balance: yup.number()
         .required('The amount of bitcoin must be entered.')
         .typeError('You must specify a number')
-        .min(1, 'The amount cannot be negative')
+        .min(1, 'The amount cannot be negative'),
+    comm_type: yup.string()
+        .required("Please select a commission type")
+        .oneOf(['Bitcoin', 'USD'], "Commission type must be either 'Bitcoin' or 'USD'"),
+
+
 })
 
 export default ClientBuyBitcoinFormSchema
