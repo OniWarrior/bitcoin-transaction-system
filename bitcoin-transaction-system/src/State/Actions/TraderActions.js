9ccb1a854e65c48eea75f47eda4ef1aae387c866
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-26 20:05:41 -0500

    created blank api call to get cancel log and dispatch start

--- bitcoin-transaction-system/src/State/Actions/TraderActions.js
+++ bitcoin-transaction-system/src/State/Actions/TraderActions.js
@@ -4,3 +4,12 @@ export const TRADER_START = 'TRADER_START'
 export const TRADER_SUCCESS = 'TRADER_SUCCESS'
 export const TRADER_FAILURE = 'TRADER_FAILURE'
 
+
+
+
+
+// api call to retrieve all records in the cancel log table
+export const getCancelLog = (navigate) => (dispatch) => {
+    dispatch({ type: TRADER_START })
+
+}
