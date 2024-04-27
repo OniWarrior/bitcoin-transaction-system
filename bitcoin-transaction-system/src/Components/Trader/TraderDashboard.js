f0e7ae2fca700194a22f09548ae6b16a6f63605c
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-26 19:08:18 -0500

    created handler for client search redirect

--- bitcoin-transaction-system/src/Components/Trader/TraderDashboard.js
+++ bitcoin-transaction-system/src/Components/Trader/TraderDashboard.js
@@ -1,7 +1,28 @@
 
+import { connect } from "react-redux"
+import React from "react";
+import SignedInNavigation from "../SignedInNavigation";
+import '../../Styles/TraderDashboard.css'
+import { useNavigate } from "react-router";
 
 
 const TraderDashboard = () => {
+    const navigate = useNavigate()
+
+    const goToCancelLog = (e) => {
+        e.preventDefault()
+        // Replace this with an api call
+        navigate('/Cancel-Log')
+    }
+
+    const goToClientSearch = (e) => {
+        e.preventDefault()
+        navigate('/TraderClientSearch')
+    }
+
+
+
+
 
 }
 
