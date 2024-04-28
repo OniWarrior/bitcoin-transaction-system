4f0f90fbcdd5acd50c09eb740b2dd8a4594a9c09
Williamnetgear <williamnetgear@yahoo.com>
2024-04-28 13:47:22 -0500

    added wallet card

--- bitcoin-transaction-system/src/Components/Client/ClientBitcoinWallet.js
+++ bitcoin-transaction-system/src/Components/Client/ClientBitcoinWallet.js
@@ -1,11 +1,20 @@
 import { connect } from "react-redux";
 import SignedInNavigation from "../SignedInNavigation";
+import '../../Styles/BitcoinWallet.css'
 
 const BitcoinWallet = (props) => {
     return (
         <div className="bitcoin-wallet-container">
             <SignedInNavigation />
-            
+            <div className="wallet-box">
+                
+                <div className="wallet-card">
+                    <h1>Current Bitcoin Balance</h1>
+                    <hr />
+                    <p>{props.client.Bitcoin_balance}</p>
+                </div>
+            </div>
+
         </div>
     )
 }
