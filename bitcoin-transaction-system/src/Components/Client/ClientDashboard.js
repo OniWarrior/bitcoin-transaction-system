817953cc4e77817233c63359e2d4979f30cb6c13
Williamnetgear <williamnetgear@yahoo.com>
2024-04-28 12:42:01 -0500

    created containers for client dash

--- bitcoin-transaction-system/src/Components/Client/ClientDashboard.js
+++ bitcoin-transaction-system/src/Components/Client/ClientDashboard.js
@@ -1,8 +1,25 @@
 import { connect } from "react-redux"
-
+import { getBitcoinWallet } from "../../State/Actions/ClientActions";
+import React from "react";
+import SignedInNavigation from "../SignedInNavigation";
+import '../../Styles/ClientDashboard.css'
+import { getPastOrders } from "../../State/Actions/ClientActions";
+import { useNavigate } from "react-router";
 
 const ClientDashboard = (props) => {
+    //local state vars
+    const navigate = useNavigate()
 
+    return (
+        <div className="client-dash">
+            <SignedInNavigation />
+            <div className="client-box">
+                <div className="client-container">
+                    
+                </div>
+            </div>
+        </div>
+    )
 }
 
 const mapStateToProps = (state) =>{
