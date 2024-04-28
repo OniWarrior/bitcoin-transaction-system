bfc9f20d61e309e29a034a9c95e60294ffdb0832
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-28 16:23:36 -0500

    made api call and inserted signed nav

--- bitcoin-transaction-system/src/Components/Trader/TraderClientSearch.js
+++ bitcoin-transaction-system/src/Components/Trader/TraderClientSearch.js
@@ -1,13 +1,50 @@
 import { connect } from "react-redux"
 import React from "react";
 import SignedInNavigation from "../SignedInNavigation";
-import '../../Styles/TraderClientSearch.css'
+import '../../Styles/Find.css'
 import { useNavigate } from "react-router";
 import { getClient } from "../../State/Actions/TraderActions";
+import { useFindValidation } from '../../Hooks/useFindValidation'
+import TraderFindFormSchema from '../../FormSchemas/TraderFindFormSchema'
+import { useState } from "react";
 
-const TraderClientSearch = () => {
+const TraderClientSearch = (props) => {
     const navigate = useNavigate()
-    const [client, errors, setClient]
+    const [client, errors, setClient] = useFindValidation(TraderFindFormSchema)
+    const initialDisabled = true
+    const [disabled, setDisabled] = useState(initialDisabled)
+
+    const change = (e) => {
+        e.preventDefault()
+        setClient(e, TraderFindFormSchema)
+    }
+
+    const handleDisabled = (event) => {
+        event.preventDefault()
+        if (client.email.length > 0) {
+            setDisabled(() => ({
+                disabled: !disabled
+            }))
+        }
+        else {
+            setDisabled(() => ({
+                disabled: disabled
+            }))
+        }
+    }
+
+    const onFormSubmit = (e) => {
+        e.preventDefault()
+        props.getClient(client, navigate)
+
+
+    }
+
+    return (
+        <div>
+            <SignedInNavigation />
+        </div>
+    )
 
 }
 
