56d50b0f5db83c22012b3cf8dd8b91e187151023
Stephen Aranda <aranda.stephen88@gmail.com>
2024-04-28 20:10:46 -0500

    completed found client

--- bitcoin-transaction-system/src/Components/Trader/TraderFoundClient.js
+++ bitcoin-transaction-system/src/Components/Trader/TraderFoundClient.js
@@ -3,8 +3,6 @@ import React from "react";
 import SignedInNavigation from "../SignedInNavigation";
 import '../../Styles/Found.css'
 import { useNavigate } from "react-router";
-import { useState } from "react";
-import { Form } from "react-bootstrap";
 import { fetchLatestCryptocurrencyForTraderBuy } from "../../State/Actions/BitcoinActions";
 import { fetchLatestCryptocurrencyForTraderSell } from "../../State/Actions/BitcoinActions";
 import { getTransfersAndTransactions } from "../../State/Actions/TraderActions";
@@ -17,26 +15,45 @@ const FoundClient = (props) => {
     // get price of bitcoin then nav to trader buy
     const goToBuy = (e) => {
         e.preventDefault()
-        props.fetchLatestCryptocurrencyForTraderBuy(navigate, props.client_id)
+        props.fetchLatestCryptocurrencyForTraderBuy(navigate, props.trader.client_id)
     }
 
     // get price of bitcoin then nav to trader sell
     const goToSell = (e) => {
         e.preventDefault()
-        props.fetchLatestCryptocurrencyForTraderSell(navigate, props.client_id)
+        props.fetchLatestCryptocurrencyForTraderSell(navigate, props.trader.client_id)
     }
 
     // get transfers and transactions and nav to cancel transfer and/or transacs
 
     const goToCancel = (e) => {
         e.preventDefault()
-        props.getTransfersAndTransactions(props.client_id, navigate)
+        props.getTransfersAndTransactions(props.trader.client_id, navigate)
     }
 
     return (
         <div>
             <SignedInNavigation />
+            <div className="client-found-box">
+                <div className="client-found-container">
+                    <div className="big-client-found-container" >
+                        <div className="big-client-info-container">
+                            <h1>Found Client</h1>
+                            <hr />
 
+                            <h2>{props.trader.first_name}</h2>
+                            <h2>{props.trader.last_name}</h2>
+                            <h2>{props.trader.email}</h2>
+                        </div>
+
+                        <div className="big-button-container">
+                            <button className="big-button" onClick={goToBuy}>Buy Bitcoin</button>
+                            <button className="big-button" onClick={goToSell}>Sell Bitcoin</button>
+                            <button className="big-button" onClick={goToCancel}>Cancel Transfer/Transaction</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
@@ -56,7 +73,8 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = {
     fetchLatestCryptocurrencyForTraderBuy,
-    fetchLatestCryptocurrencyForTraderSell
+    fetchLatestCryptocurrencyForTraderSell,
+    getTransfersAndTransactions
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(FoundClient)
