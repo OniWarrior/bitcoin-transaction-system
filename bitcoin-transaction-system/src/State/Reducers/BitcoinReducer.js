aa9c3b366f34a9ed57326bd927e61213e5a871ea
kemon128 <DavidPayare1124@outlook.com>
2024-04-30 15:51:09 -0500

    Update BitcoinReducer.js

--- bitcoin-transaction-system/src/State/Reducers/BitcoinReducer.js
+++ bitcoin-transaction-system/src/State/Reducers/BitcoinReducer.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import {
     BITCOIN_START,
     BITCOIN_SUCCESS,
@@ -11,14 +12,37 @@ const initialState = {
 }
 
 export const bitcoinReducer = (state=initialState,action)=>{
+=======
+import{
+    BITCOIN_START,
+    BITCOIN_SUCCESS,
+    BITCOIN_FAILURE
+}from '../Actions/BitcoinActions'
+
+const initialState={
+    bitcoin:{},
+    loading: '',
+    error:''
+}
+
+
+
+export const bitcoinReducer=(state=initialState,action)=>{
+>>>>>>> Stashed changes
     switch(action.type){
         case BITCOIN_START:
             return{...state,loading:true}
         case BITCOIN_SUCCESS:
             return{...state,bitcoin:action.payload,loading:false}
         case BITCOIN_FAILURE:
+<<<<<<< Updated upstream
             return{...state,error:action.payload,loading:false}
         default:
             return state
+=======
+            return{...state,error:action.payload, loading:false}
+        default:
+         return state   
+>>>>>>> Stashed changes
     }
 }
