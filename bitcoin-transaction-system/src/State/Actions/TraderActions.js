af5076e615f36a4e36edd1ff82ff008ee080337a
CabanDiego <majy2098@gmail.com>
2024-04-29 21:15:08 -0500

    created blank api call to get cancel log and dispatch call

--- bitcoin-transaction-system/src/State/Actions/TraderActions.js
+++ bitcoin-transaction-system/src/State/Actions/TraderActions.js
@@ -3,3 +3,11 @@ import axiosWithAuth from "../../Components/Utils?axioswithAuth"
 export const TRADER_START = 'TRADER_START'
 export const TRADER_SUCCESS = 'TRADER_SUCCESS'
 export const TRADER_FAILURE = 'TRADER_FAILURE'
+
+
+
+
+//api call to retrieve all records in the cancel log table
+export const getCancelLog=(navigate) =>(dispatch)=>{
+    dispatch(type: TRADER_START)
+}
