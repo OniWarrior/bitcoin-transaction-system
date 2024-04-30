507e91ae81c43196f10eb58acbac87ee0006e778
CabanDiego <majy2098@gmail.com>
2024-04-29 21:05:25 -0500

    created strings for dispatch to action

---
+++ bitcoin-transaction-system/src/State/Actions/TraderActions.js
@@ -0,0 +1,5 @@
+import axiosWithAuth from "../../Components/Utils?axioswithAuth"
+
+export const TRADER_START = 'TRADER_START'
+export const TRADER_SUCCESS = 'TRADER_SUCCESS'
+export const TRADER_FAIL = 'TRADER_FAIL'
