be06365b16397e9b5cccd0b34932ae9b2d826616
kemon128 <DavidPayare1124@outlook.com>
2024-04-30 15:51:17 -0500

    Update BitcoinActions.js

--- bitcoin-transaction-system/src/State/Actions/BitcoinActions.js
+++ bitcoin-transaction-system/src/State/Actions/BitcoinActions.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import axios from 'axios'
 
 export const BITCOIN_START = 'BITCOIN_START'
@@ -20,4 +21,38 @@ export const fetchLastestCryptocurrency = () => (dispatch) => {
         .catch(error => {
             dispatch({type: BITCOIN_FAILURE, payload: error.message });
         });
+=======
+import axiosWithAuth from "../../Components/Utils/AxiosWithAuth"
+export const BITCOIN_START='BITCOIN_START'
+export const BITCOIN_SUCCESS='BITCOIN_SUCCESS'
+export const BITCOIN_FAILURE='BITCOIN_FAILURE'
+
+
+
+
+export const fetchLatestCryptoCurrencyForClient=(navigate)=>(dispatch)=>{
+    dispatch({type:BITCOIN_START});
+    axiosWithAuth().get('/api/users/latest',)
+    .then(response=>{
+       dispatch({type:BITCOIN_SUCCESS,payload:response.data})
+       navigate('/ClientDashboard/BuyBitcoin')
+    })
+    .catch(error=>{
+        dispatch({type:BITCOIN_FAILURE,payload:error.message});
+    });
+}
+
+
+
+export const fetchLatestCryptoCurrencyForTrader=(navigate,clientId)=>(dispatch)=>{
+   dispatch({type:BITCOIN_START}); 
+   axiosWithAuth().get('/api/users/latest',)
+    .then(response=>{
+        dispatch({type:BITCOIN_SUCCESS,payload:response.data});
+        navigate('/TraderClientSearch/clients/${clientId}/TraderBuyBitcoin');
+    })
+    .catch(error=>{
+        dispatch({type:BITCOIN_FAILURE,payload:error.message});
+    });
+>>>>>>> Stashed changes
 }
