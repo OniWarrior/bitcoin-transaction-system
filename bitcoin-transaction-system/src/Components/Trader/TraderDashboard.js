e0072b56b8d4fb1fe85f54aeb8973f6a5db8613d
Williamnetgear <williamnetgear@yahoo.com>
2024-04-30 17:52:39 -0500

    completed TraderFindFormSchema.js

--- bitcoin-transaction-system/src/Components/Trader/TraderDashboard.js
+++ bitcoin-transaction-system/src/Components/Trader/TraderDashboard.js
@@ -15,7 +15,7 @@ const TraderDashboard = (props) => {
 
     const goToClientSearch=(e)=>{
         e.preventDefault()
-        navigate('/TraderClientSearch')
+        navigate('/TraderDashboard/TraderClientSearch')
     }
 
     return(
