533d0f07fd67e3a5c32cb4c4f533101570cb567f
Williamnetgear <williamnetgear@yahoo.com>
2024-04-29 21:06:15 -0500

    created past orders list

--- bitcoin-transaction-system/src/Components/Client/ClientPastOrders.js
+++ bitcoin-transaction-system/src/Components/Client/ClientPastOrders.js
@@ -1,7 +1,25 @@
 import { connect } from "react-redux"
+import PastOrderCard from "./PastOrder"
+import SignedInNavigation from "../SignedInNavigation"
 
 const PastOrders = (props) => {
-
+    if(!props.client || props.client.length===0)
+    {
+        return <div>No Past Orders</div>
+    }
+    return (
+        <div className="past-orders-list">
+            <SignedInNavigation />
+            <div className="past-orders-list">
+                <h1>Past Orders</h1>
+                {
+                    props.client.map(order=>{
+                        <PastOrderCard key={order.order_id} client={order}/>
+                    })
+                }
+            </div>
+        </div>
+    )
 }
 
 const mapStateToProps = (state) =>{
