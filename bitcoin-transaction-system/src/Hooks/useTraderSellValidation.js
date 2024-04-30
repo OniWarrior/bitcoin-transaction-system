ed24393d1394c6c03f92a32aec29616f5442d2fa
Williamnetgear <williamnetgear@yahoo.com>
2024-04-30 16:54:33 -0500

    completed trader sell

---
+++ bitcoin-transaction-system/src/Hooks/useTraderSellValidation.js
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import * as yup from 'yup'
+
+const initialValues = {
+    Bitcoin_balance: '',
+    Bitcoin_price: '',
+    email: '',
+}
+
+const initialErrors = {
+    Bitcoin_balance: '',
+    Bitcoin_price: '',
+    email: '',
+}
+
+export const useTraderSellBitcoinValidation = (schema) =>{
+    const[data, setData] = useState(initialValues)
+    const[errors,setErrors] = useState(initialErrors)
+
+    const onInputChange = (event) =>{
+        const{name,value} = event.target
+        yup.reach(schema, name)
+            .validate(value)
+            .then(()=>{
+                setErrors({...errors, [name]: ""})
+            })
+            .catch(err=>{
+                setErrors({...errors, [name]:err.errors[0]})
+            })
+
+        setData({
+            ...data,
+            [name]: value
+        })
+    };
+
+    return [data, errors, onInputChange]
+}
