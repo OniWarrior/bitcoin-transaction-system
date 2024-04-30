5999ffd2c6ce5556f5477f58c0ea7d254c7b5f8f
Williamnetgear <williamnetgear@yahoo.com>
2024-04-29 20:55:31 -0500

    added PastOrderCard.css and PastOrder.js

--- bitcoin-transaction-system/src/Components/Client/PastOrder.js
+++ bitcoin-transaction-system/src/Components/Client/PastOrder.js
@@ -1,13 +1,27 @@
 import React from "react";
 import { connect } from "react-redux";
+import '../../Styles/PastOrderCard.css'
 
 const PastOrderCard=(props)=>{
+    return(
+        <div className="order-card">
+            <h2>Past Order</h2>
+            <p>{props.client.order_id}</p>
+            <p>{props.client.client_id}</p>
+            <p>{props.client.date}</p>
+            <p>{props.client.comm_paid}</p>
+            <p>{props.client.comm_type}</p>
+            <p>{props.client.Bitcoin_balance}</p>
 
+        </div>
+    )
 }
 
 const mapStateToProps=(state)=>{
     return{
         client:state.clientReducer.client,
-        loading:state.clientReducer
+        loading:state.clientReducer.loading,
+        error:state.clientReducer.error
     }
 }
+export default connect(mapStateToProps)(PastOrderCard)
