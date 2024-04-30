c0aad92fab6e1692442e25fdd87ddb37037fa180
CabanDiego <majy2098@gmail.com>
2024-04-29 20:16:00 -0500

    created handler for client search redirect

--- bitcoin-transaction-system/src/Components/Trader/TraderDashboard.js
+++ bitcoin-transaction-system/src/Components/Trader/TraderDashboard.js
@@ -1,7 +1,24 @@
-
-
+import { connect } from "react-redux"
+import React from "react";
+import SignedInNavigation from "../SignedInNavigation";
+import '../../Styles/TraderDashboard.css'
+import { useNavigate } from "react-router";
 
 const TraderDashboard = () => {
+    const navigate = useNavigate()
+
+    const goToCancelLog=(e)=>{
+        e.preventDefault()
+        //Replace with api call
+        navigate('/Cancel-log')
+    }
+
+    const goToClientSearch=(e)=>{
+        e.preventDefault()
+        navigate('/TraderClientSearch')
+    }
+
+    
 
 }
 
